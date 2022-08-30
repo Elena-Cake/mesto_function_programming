a91812ce0d4bfb825626e64a531eb55c6753d19e
Elena Sveshnikova <lenamarinkevich@mail.ru>
2022-08-30 13:50:20 +0300

    fear: popap name change

---
+++ index.js
@@ -0,0 +1,27 @@
+
+// работа с формой
+
+let formElement =  document.querySelector('.edit__form');
+// Находим поля формы в DOM
+let nameInput = document.querySelector('.edit__input-name');
+let jobInput = document.querySelector('.edit__input-job');
+//Находим значения profile
+let titleName = document.querySelector('.profile__name');
+let titleJob = document.querySelector('.profile__job');
+
+//присвоим значения title инпутам
+nameInput.placeholder = titleName.textContent;
+jobInput.placeholder = titleJob.textContent;
+
+
+// Обработчик «отправки» формы
+function formSubmitHandler (event) {
+    event.preventDefault(); 
+
+    titleName.textContent = nameInput.value;
+    titleJob.textContent = jobInput.value;
+}
+formElement.addEventListener('submit', formSubmitHandler); 
+
+
+
