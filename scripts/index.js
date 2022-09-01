b8e34eb7173ed5cbf061f9ffff1e47de31887e60
Elena Sveshnikova <lenamarinkevich@mail.ru>
2022-09-01 17:08:24 +0300

    popup broke

--- scripts/index.js
+++ scripts/index.js
@@ -1,51 +1,49 @@
 //_____________________________
-// РАБОТА С ФОРМОЙ
+//  РАБОТА С ФОРМОЙ
 //_____________________________
 
 let formElement =  document.querySelector('.popup__form');
 let btnSave = document.querySelector('.popup__save');
-// Находим поля формы в DOM
+// находим поля формы в DOM
 let nameInput = document.querySelector('.popup__input_name');
 let jobInput = document.querySelector('.popup__input_job');
-//Находим значения profile
+// находим значения profile
 let titleName = document.querySelector('.profile__name');
 let titleJob = document.querySelector('.profile__job');
-//для вывода и скрытия попап в видимой зоне
+// для вывода и скрытия попап в видимой зоне
 let btnEdit = document.querySelector('.profile__edit');
 let popup = document.querySelector('.popup');
 let btnClose = document.querySelector('.popup__close');
 
-// Скрываю форму при загрузке страницы
+// скрываю форму при загрузке страницы
 closePopup();
 
-//добавляем и скрываем попап
-function addPopup() {
+// добавляем попап
+function openPopup() {
      popup.classList.add('popup_opened');
     //присвоим значения title инпутам
     nameInput.value = titleName.textContent;
     jobInput.value = titleJob.textContent;
     nameInput.focus();
 };
+// скрываем попап
 function closePopup() {
     popup.classList.remove('popup_opened');
 };
 
-//нажатие кнопки редактирования
-btnEdit.addEventListener('click', addPopup);
+// нажатие кнопки редактирования
+btnEdit.addEventListener('click', openPopup);
 
-//нажатие крестика
-btnClose.addEventListener('click', closePopup);
-
-
-// Обработчик «отправки» формы
+// обработчик «отправки» формы
 function formSubmitHandler (event) {
     event.preventDefault(); 
-    if (nameInput.value !== '') {
     titleName.textContent = nameInput.value;
-    };
     titleJob.textContent = jobInput.value;
     closePopup();
 
 }
-btnSave.addEventListener('click', formSubmitHandler); 
+btnSave.addEventListener('submit', formSubmitHandler); 
+
+// нажатие крестика
+btnClose.addEventListener('click', closePopup);
 
