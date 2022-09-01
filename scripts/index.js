62faf8c47f1a47a51d11ac6f26a7d6099874d719
Elena Sveshnikova <lenamarinkevich@mail.ru>
2022-09-01 12:05:36 +0300

    fix: mistakes and refuctor: js

--- scripts/index.js
+++ scripts/index.js
@@ -4,36 +4,36 @@
 let formElement =  document.querySelector('.popup__form');
 let btnSave = document.querySelector('.popup__save');
 // Находим поля формы в DOM
-let nameInput = document.querySelector('.popup__input-name');
-let jobInput = document.querySelector('.popup__input-job');
+let nameInput = document.querySelector('.popup_type_name');
+let jobInput = document.querySelector('.popup_type_job');
 //Находим значения profile
 let titleName = document.querySelector('.profile__name');
 let titleJob = document.querySelector('.profile__job');
+//для вывода и скрытия попап в видимой зоне
+let btnEdit = document.querySelector('.profile__edit');
+let popup = document.querySelector('.popup');
+let btnClose = document.querySelector('.popup__close');
 
 //присвоим значения title инпутам
 nameInput.value = titleName.textContent;
 jobInput.value = titleJob.textContent;
 
+// Скрываю форму при загрузке страницы
+closePopup();
+
+//добавляем и удаляем попап из видимой части
+function addPopup() {
+     popup.classList.add('popup_opened');
+};
+function closePopup() {
+    popup.classList.remove('popup_opened');
+};
 
 //нажатие кнопки редактирования
-let popupButton = document.querySelector('.profile__edit');
-let popup = document.querySelector('.popup');
-
-
-popupButton.addEventListener('click', ()=> {
-    popup.classList.add('popup_opened');
-});
-
-
-// Скрываю форму при загрузке страницы
-    popup.classList.remove('popup_opened');
-
+btnEdit.addEventListener('click', addPopup);
 
 //нажатие крестика
-let closeButton = document.querySelector('.popup__close')
-closeButton.addEventListener('click', ()=> {
-    popup.classList.remove('popup_opened')
-});
+btnClose.addEventListener('click', closePopup);
 
 
 // Обработчик «отправки» формы
@@ -41,7 +41,7 @@ function formSubmitHandler (event) {
     event.preventDefault(); 
     titleName.textContent = nameInput.value;
     titleJob.textContent = jobInput.value;
-    popup.classList.remove('popup_opened');
+    closePopup();
 
 }
 btnSave.addEventListener('click', formSubmitHandler); 
