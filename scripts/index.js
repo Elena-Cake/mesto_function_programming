8545200639de7491231946c862dfaed0de422f4c
Elena Sveshnikova <lenamarinkevich@mail.ru>
2022-09-13 13:03:06 +0300

    fear: template cards

--- scripts/index.js
+++ scripts/index.js
@@ -14,6 +14,8 @@ const titleJob = document.querySelector('.profile__job');
 const btnEdit = document.querySelector('.profile__btn-edit');
 const popup = document.querySelector('.popup');
 const btnClose = document.querySelector('.popup__btn-close');
+// карточки
+const cardsContainer = document.querySelector('.elements')
 
 // скрываю форму при загрузке страницы
 closePopup();
@@ -34,16 +36,74 @@ function closePopup() {
 // нажатие кнопки редактирования
 btnEdit.addEventListener('click', openPopup);
 
-// // обработчик «отправки» формы
+// обработчик «отправки» формы
 function formSubmitHandler (event) { 
     event.preventDefault(); 
     titleName.textContent = nameInput.value; 
     titleJob.textContent = jobInput.value; 
     closePopup();
 } 
-formElement.addEventListener('submit', formSubmitHandler);  
+// formElement.addEventListener('submit', formSubmitHandler);  
 
  // нажатие крестика
-btnClose.addEventListener('click', closePopup);
+// btnClose.addEventListener('click', closePopup);
 
  
+//_____________________________
+//  РАБОТА С КАРТОЧКАМИ
+//_____________________________
+
+// карточки по умолчанию
+const initialCards = [
+    {
+      name: 'Архыз',
+      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg'
+    },
+    {
+      name: 'Челябинская область',
+      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/chelyabinsk-oblast.jpg'
+    },
+    {
+      name: 'Иваново',
+      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/ivanovo.jpg'
+    },
+    {
+      name: 'Камчатка',
+      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kamchatka.jpg'
+    },
+    {
+      name: 'Холмогорский район',
+      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kholmogorsky-rayon.jpg'
+    },
+    {
+      name: 'Байкал',
+      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg'
+    }
+  ]; 
+
+// ф-ция добавления карточки
+  function addCard(name, link) {
+    const cardsTemplate = document.querySelector('#cards').content;
+    const cardElement = cardsTemplate.querySelector('.element').cloneNode(true);
+  
+    cardElement.querySelector('.element__name').textContent = name;
+    cardElement.querySelector('.element__foto').src = link;
+    // лайк
+    cardElement.querySelector('.element__btnLike').addEventListener ('click', function (evt) {
+        evt.target.classList.toggle('element__btnLike_active')
+      });
+    //   удаляем карточку
+    cardElement.querySelector('.element__btnTrash').addEventListener ('click', () => cardElement.closest('.element').remove());
+
+    cardsContainer.prepend(cardElement)
+  }
+
+// добавляю массив фоток
+  initialCards.forEach(item => addCard(item.name , item.link))
+
+
+// добавление по кнопке + попап
+
+
+
+
