e26859e49d7744f59bd395c0cac8a303b9cf8a70
Elena Sveshnikova <lenamarinkevich@mail.ru>
2022-09-28 16:25:13 +0300

    fear: стили написала

--- scripts/index.js
+++ scripts/index.js
@@ -156,13 +156,13 @@ const showInputError = (formElement, inputElement, errorMessage) => {
   const errorElement = formElement.querySelector(`.${inputElement.id}-error`);
   inputElement.classList.add('popup__input_type_error');
   errorElement.textContent = errorMessage;
-  errorElement.classList.add('form__input-error_active');
+  errorElement.classList.add('popup__input-error_active');
 };
 
 const hideInputError = (formElement, inputElement) => {
   const errorElement = formElement.querySelector(`.${inputElement.id}-error`);
-  inputElement.classList.remove('form__input_type_error');
-  errorElement.classList.remove('form__input-error_active');
+  inputElement.classList.remove('popup__input_type_error');
+  errorElement.classList.remove('popup__input-error_active');
   errorElement.textContent = '';
 };
 
