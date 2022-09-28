fa806a9de1f666fe4f704e6bf5b5b1c14c02169b
Elena Sveshnikova <lenamarinkevich@mail.ru>
2022-09-28 13:28:37 +0300

    fear: стили написала

--- scripts/index.js
+++ scripts/index.js
@@ -147,3 +147,71 @@ buttonCloseAddFoto.addEventListener('click', () => {
   closePopup(popupAddCard); 
   formElementAddFoto.reset();
 });
+
+//_____________________________
+//  ВАЛИДАЦИЯ
+//_____________________________
+// span ошибки
+const showInputError = (formElement, inputElement, errorMessage) => {
+  const errorElement = formElement.querySelector(`.${inputElement.id}-error`);
+  inputElement.classList.add('popup__input_type_error');
+  errorElement.textContent = errorMessage;
+  errorElement.classList.add('form__input-error_active');
+};
+
+const hideInputError = (formElement, inputElement) => {
+  const errorElement = formElement.querySelector(`.${inputElement.id}-error`);
+  inputElement.classList.remove('form__input_type_error');
+  errorElement.classList.remove('form__input-error_active');
+  errorElement.textContent = '';
+};
+
+// проверка на валидность
+const checkInputValidity = (formElement, inputElement) => {
+  if (!inputElement.validity.valid) {
+    showInputError(formElement, inputElement, inputElement.validationMessage);
+  } else {
+    hideInputError(formElement, inputElement);
+  }
+};
+
+const setEventListeners = (formElement) => {
+  const inputList = Array.from(formElement.querySelectorAll('.popup__input'));
+  const buttonElement = formElement.querySelector('.popup__btn-save');
+  toggleButtonState(inputList,buttonElement);
+  inputList.forEach((inputElement) => {
+    inputElement.addEventListener('input', function () {
+      checkInputValidity(formElement, inputElement);
+      toggleButtonState(inputList,buttonElement);
+    });
+  });
+};
+
+const enableValidation = () => {
+  const formList = Array.from(document.querySelectorAll('.popup__form'));
+  formList.forEach((formElement) => {
+    formElement.addEventListener('submit', function (evt) {
+      evt.preventDefault();
+    });
+    
+    const fieldsetList = Array.from(formElement.querySelectorAll('.popup'));
+    fieldsetList.forEach((fieldSet) => {
+      setEventListeners(fieldSet);
+    }); 
+  });
+};
+enableValidation();
+
+function hasInvalidInput(inputList) {
+  return inputList.some((inputElement)=>{
+    return !inputElement.validity.valid;
+  })
+}
+
+function toggleButtonState (inputList, buttonElement) {
+  if (hasInvalidInput(inputList)){
+    buttonElement.classList.add('btn-save_inactive')
+  } else {
+    buttonElement.classList.remove('btn-save_inactive')
+  }
+}
